6eea544c2048d9f5c2cd3f9c57d96bebf555f798
LuizED <eduardol@fastmail.com>
2022-09-16 15:41:28 -0300

    add a getComputerChoice and randomNumber function

--- js/code.js
+++ js/code.js
@@ -1,1 +1,14 @@
 console.log("Hello World!!!!!!!!!!!!");
+
+function getComputerChoice(){
+ switch(randomNumber()){
+    case 1:
+        return "rock";
+    case 2:
+        return "scissor";
+    case 3:
+        return "paper"; 
+ }
+}
+// return a random number between 1 and 3 inclusive
+let randomNumber = () => Math.floor( Math.random()*3 +1);
